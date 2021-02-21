ff53d30b592f1aed4fef1c6fe790a07ca8277f02
Moi Myazz <moimyazz@gmail.com>
2021-02-21 04:32:57 -0500

    ALLOWED_HOSTS Updated

--- GDM/settings.py
+++ GDM/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gdm-tech.herokuapp.com']
 
 
 # Application definition
