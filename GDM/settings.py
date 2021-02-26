25d30a46dcffba254c74c38c4eccafb3ab78d7ad
Moi Myazz <moimyazz@gmail.com>
2021-02-25 20:35:32 -0500

    Send email from comment model settings

--- GDM/settings.py
+++ GDM/settings.py
@@ -152,12 +152,13 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Email setup
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 # All sessions will be expired if the user closes their browser
 # SESSION_EXPIRE_AT_BROWSER_CLOSE = True
